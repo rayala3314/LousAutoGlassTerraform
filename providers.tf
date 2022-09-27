ac084cf943cdebe0c8fab06e63da0c16e5631db3
Raymundo Ayala <rayala19@csub.edu>
2022-09-27 15:07:13 -0700

    test

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = var.region
-  # version = "~> 3.0"
-  required_version  = "~> 3.0"
+  version = "~> 3.0"
+  # required_version  = "~> 3.0"
   profile           = "lousautoglass"
 }
