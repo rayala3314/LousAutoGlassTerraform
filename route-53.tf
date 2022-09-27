227a7d748dfc48d6e2a79bb14445b1f7ab0815c1
Raymundo Ayala <rayala19@csub.edu>
2022-09-27 15:10:24 -0700

    Added records

--- route-53.tf
+++ route-53.tf
@@ -1,15 +1,15 @@
 # get hosted zone details
 # terraform aws data hosted zone
-# data "aws_route53_zone" "hosted_zone" {
-#   name = var.domain_name
-# }
+data "aws_route53_zone" "hosted_zone" {
+  name = var.domain_name
+}
 
-# # create a record set in route 53
-# # terraform aws route 53 record
-# resource "aws_route53_record" "site_domain" {
-#   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-#   name    = var.record_name
-#   type    = "A"
-#   ttl     = "300"
-#   records = [aws_instance.lous_autoglass.public_ip]
-# }
+# create a record set in route 53
+# terraform aws route 53 record
+resource "aws_route53_record" "site_domain" {
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = var.record_name
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.lous_autoglass[0].public_ip]
+}
